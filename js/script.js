92c6ad376fe8489eb27f6b8a4f29b3f80a594b25
Alessandro <alessandro01desideri@gmail.com>
2021-12-13 22:05:10 +0100

    Funzioni per stamp Fizz,Buzz e FizzBuzz

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 // Ciclo 'for' stampa numeri
+for (let i = 1; i <=100; i++) {
+console.log(i);
 
 // Funzione per stampare Fizz Buzz o FizzBuzz
 
+// Stampa Fizz se multipli di 3
+if (i % 3 === 0) {  
+    console.log('Fizz');
+
+// Stampa Buzz se multipli di 5
+} else if (i % 5 === 0) {
+    console.log('Buzz');
+
+// Stampa FizzBuzz se multipli di 3 e 5
+} else if (i % 3 === 0 && i % 5 === 0) {  
+    console.log('FizzBuzz');
+} else {
+    console.log(i);
+}
 // Costante per container e ciclo for per stamp DOM
 
 // Ciclo for
