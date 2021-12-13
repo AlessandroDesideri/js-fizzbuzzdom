5f52aa1c03605de731c9e81587f96ec0a42dc7c0
Alessandro <alessandro01desideri@gmail.com>
2021-12-13 22:12:52 +0100

    Aggiunte funzioni stamp DOM

--- js/script.js
+++ js/script.js
@@ -18,6 +18,27 @@ if (i % 3 === 0) {
 } else {
     console.log(i);
 }
-// Costante per container e ciclo for per stamp DOM
 
-// Ciclo for
+// Inizializzo costante per container e ciclo for stamp DOM
+const containerHtml = document.querySelector('.container-boxes');
+
+// Ciclo for DOM
+for (let i = 1; i <=100; i++) {
+
+    // Stampa Fizz nel DOM se multiplo di 3
+    if (i % 3 === 0) {  
+    containerHtml.innerHTML += `<div class="box box-fizz">Fizz</div>`;
+
+    // Stampa Buzz nel DOM se multiplo di 5
+    } else if (i % 5 === 0) {  
+        containerHtml.innerHTML += `<div class="box box-buzz">Buzz</div>`;
+
+    // Stampa FizzBuzz se multiplo di 3 e 5
+    }else if (i % 3 === 0 && i % 5 === 0) {  
+            containerHtml.innerHTML += `<div class="box box-fizzbuzz">FizzBuzz</div>`
+    
+    // Stampa nel DOM la successione dei numeri
+    } else {
+        containerHtml.innerHTML += `<div class="box">${i}</div>`; 
+    }
+}
